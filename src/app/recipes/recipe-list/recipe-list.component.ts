81736118717e4f2675a7f10f6b0585ef8f5048f8
Ashwin <ashwingairola999@gmail.com>
2021-06-07 04:05:30 +0530

    53. Adding Content to the RecipesComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
 	selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-	recipes = [];
+	recipes: Recipe[] = [
+		new Recipe(
+			'A Test Recipe',
+			'This is simply a test.',
+			'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Faseatenontv.files.wordpress.com%2F2013%2F03%2Fkrabby-patty-2.jpg&f=1&nofb=1'
+		)
+	];
 
 	constructor() {}
 
