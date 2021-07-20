a9f83e34fd08a71508425994a7e4e0df7f13d1c7
Ashwin <ashwingairola999@gmail.com>
2021-07-20 22:53:18 +0530

    165. Retrieving Route Parameters

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { delay, map, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-recipe-edit',
 	templateUrl: './recipe-edit.component.html',
 	styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
-	constructor() {}
+export class RecipeEditComponent implements OnInit, OnDestroy {
+	constructor(private route: ActivatedRoute) {}
 
-	ngOnInit(): void {}
+	private destroySubject$ = new Subject<void>();
+	editMode = false;
+	recipeId: number | null = null;
+
+	ngOnInit(): void {
+		this.route.paramMap
+			.pipe(delay(3000), takeUntil(this.destroySubject$))
+			.subscribe(params => {
+				const id = params.get('id');
+				this.recipeId = id ? +id : null;
+				this.editMode = id !== null;
+				console.log(this.recipeId, this.editMode);
+			});
+	}
+
+	ngOnDestroy() {
+		this.destroySubject$.next();
+	}
 }
