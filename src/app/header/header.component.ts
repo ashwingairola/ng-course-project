9d9921c6bc8932947fcfa86d7889028fb5e5d83f
Ashwin <ashwingairola999@gmail.com>
2021-08-22 15:45:01 +0530

    280. Storing Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { noop } from 'rxjs';
+import { DataStorageService } from '../services/api/data-storage.service';
 
 @Component({
 	selector: 'app-header',
@@ -6,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-	constructor() {}
+	constructor(private dataStorageService: DataStorageService) {}
 
 	ngOnInit(): void {}
+
+	onSaveData() {
+		this.dataStorageService.storeRecipes().subscribe(response => {
+			console.log(response);
+		}, noop);
+	}
 }
--- src/app/services/api/data-storage.service.ts
+++ src/app/services/api/data-storage.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipesService } from '../recipes.service';
+import { switchMap, take } from 'rxjs/operators';
+
 @Injectable({
 	providedIn: 'root'
 })
 export class DataStorageService {
-	constructor(private http: HttpClient) {}
+	constructor(
+		private http: HttpClient,
+		private recipesService: RecipesService
+	) {}
+
+	storeRecipes() {
+		return this.recipesService.getRecipes().pipe(
+			take(1),
+			switchMap(recipes => {
+				return this.http.put<unknown>(
+					'https://ng-course-project-89e65-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json',
+					recipes
+				);
+			})
+		);
+	}
 }
