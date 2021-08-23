1b3cf63e1138525f8625ed409a76c69b8fb55aa2
Ashwin <ashwingairola999@gmail.com>
2021-08-23 13:49:24 +0530

    Sending the Signup Request

--- src/app/routes/auth/components/auth/auth.component.ts
+++ src/app/routes/auth/components/auth/auth.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { noop } from 'rxjs';
+
+import { AuthService } from '../../../../services/api/auth.service';
 
 @Component({
 	selector: 'app-auth',
@@ -9,7 +12,7 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
 	@ViewChild('authForm') authForm!: NgForm;
 
-	constructor() {}
+	constructor(private authService: AuthService) {}
 
 	isLoginMode = true;
 
@@ -20,7 +23,20 @@ export class AuthComponent implements OnInit {
 	}
 
 	onSubmit() {
-		console.log(this.authForm.value);
-		this.authForm.reset();
+		if (this.authForm.invalid) {
+			return;
+		}
+
+		const email = this.authForm.value.email;
+		const password = this.authForm.value.password;
+
+		if (this.isLoginMode) {
+			return;
+		} else {
+			this.authService.signUp(email, password).subscribe(response => {
+				console.log(response);
+				this.authForm.reset();
+			}, noop);
+		}
 	}
 }
